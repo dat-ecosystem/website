bacb0c4685b4880e156640721c38a3402fe7639d
Mehrab Gazbur <mehrab-98@hotmail.com>
2023-05-19 02:01:03 +0500

    coded - main page

--- page/page.js
+++ page/page.js
@@ -1,1 +1,17 @@
-document.body.innerHTML = `<h1>Initial page</h1>`
+const navbar = require('../src/node_module/navbar/index.js')
+
+const title = document.createElement('h1')
+title.innerHTML = 'Icon button'
+const navbar_component = navbar()
+
+const main = document.createElement('div')
+
+
+// Setting padding and margin to 0px
+document.documentElement.style.margin = `0`
+document.documentElement.style.padding = `0`
+document.body.style.margin = `0`
+document.body.style.padding = `0`
+
+main.append(navbar_component)
+document.body.append(main)
