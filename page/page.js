59dfaaa5a5522780639796c26e6d1b15581218ea
serapath <dev@serapath.de>
2023-08-30 16:12:53 +0200

    refactor desktop.js to not mutate the document directly, but to return an element instead

--- page/page.js
+++ page/page.js
@@ -1,4 +1,3 @@
-const desktop = require('..')
 /******************************************************************************
   CSS & HTML Defaults
 ******************************************************************************/
@@ -13,20 +12,26 @@ const desktop = require('..')
   const font_url = 'https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap'
   const html_string = `<link rel="stylesheet" type="text/css" href="${font_url}" ></link>`
   const [link] = Object.assign(document.createElement('div'), { innerHTML: html_string }).children
-
-  document.head.append(meta, link)
+  const onfontload = new Promise(ok => link.onload = ok)
 
   const sheet = new CSSStyleSheet()
   sheet.replaceSync(`html, body { padding:0px; margin: 0px; }`)
+
+  document.head.append(meta, link)
   document.adoptedStyleSheets = [sheet]
+  onfontload.then(boot)
 }
 /******************************************************************************
   INITIALIZE PAGE
 ******************************************************************************/
-const light_theme = require('theme/lite-theme')
-const dark_theme = require('theme/dark-theme')
-
-console.log({light_theme, dark_theme})
-const el = desktop()
-
-// document.body.append(el)
+function boot () {
+  const desktop = require('..')
+  const light_theme = require('theme/lite-theme')
+  const dark_theme = require('theme/dark-theme')
+  
+  console.log({light_theme, dark_theme})
+  
+  const el = desktop()
+  const shadow = document.body.attachShadow({ mode: 'closed' })
+  shadow.append(el)
+}
--- src/desktop.js
+++ src/desktop.js
@@ -6,51 +6,79 @@ const consortium_page = require('consortium_page')
 const terminal = require('terminal')
 const navbar = require('navbar')
 
+// DEFAULT THEMES
 const light_theme = require('theme/lite-theme')
 const dark_theme = require('theme/dark-theme')
 
-// Default Theme
-let current_theme = light_theme
 const sheet = new CSSStyleSheet()
+let current_theme = light_theme
 sheet.replaceSync(get_theme(current_theme))
 
 module.exports = desktop
-
-function desktop () {
-  //Default Page
-  let current_page = consortium_page({data: current_theme})
-  let notify
+/******************************************************************************
+  DESKTOP COMPONENT
+******************************************************************************/
+function desktop (opts = {}, protocol) {
+  // ----------------------------------------
+  // SETUP
+  // ----------------------------------------
+  let notify // remove
+  // ----------------------------------------
+  // TEMPLATE
+  // ----------------------------------------
+  const el = document.createElement('div')
+  const sh = el.attachShadow({ mode: 'closed' })
+  sh.innerHTML = `
+    <div class="navbar"></div>
+    <div class="content"></div>
+    <div class="shell"></div>
+  `
+  sh.adoptedStyleSheets = [sheet]
+  const [nav, content, terminal_wrapper] = sh.children
+  // ----------------------------------------
+  // NAVBAR
+  // ----------------------------------------
   const PROTOCOL = {
-    'active_page': 'HOME',
+    'active_page': 'HOME', // @TODO: remove
     'handle_page_change': handle_page_change,
     'handle_theme_change': handle_theme_change,
     'toggle_terminal': toggle_terminal
   }
-
-  const terminal_wrapper = terminal({ data: current_theme })
-
+  const navbar_opts = { page: opts.page, data: current_theme }
+  // nav.attachShadow({ mode: 'closed' }).append(navbar(navbar_opts, navbar_protocol))
+  nav.attachShadow({ mode: 'closed' }).append(navbar(navbar_opts, page_protocol))
+  // ----------------------------------------
+  // CONTENT
+  // ----------------------------------------
   const page_list = {
+    // @TODO: maybe only store "factories" and create instance on demand?
     'HOME': home_page({ data: current_theme }),
     'PROJECTS': projects_page({ data: current_theme }),
     'GROWTH PROGRAM': growth_page({ data: current_theme }),
     'TIMELINE': timeline_page({ data: current_theme }),
-    'DEFAULT': consortium_page({ data: current_theme })
-  }
-
-  const page_filler = document.createElement('div')
-  page_filler.classList.add('page_filler')
-  document.body.append(page_filler, navbar({data: current_theme}, page_protocol), current_page)
+    'DEFAULT': consortium_page({ data: current_theme }),
+  } // @INFO: the initial visible content is set when receiving the first navbar message
+  const content_sh = content.attachShadow({ mode: 'closed' })
+  // ----------------------------------------
+  // TERMINAL
+  // ----------------------------------------
+  const terminal_sh = terminal_wrapper.attachShadow({ mode: 'closed' })
+  const terminal_el = terminal({ data: current_theme })
+  // ----------------------------------------
+  // INIT
+  // ----------------------------------------
+  //Default Page
+  let current_page = consortium_page({data: current_theme})
   handle_page_change('DEFAULT')
-
   // Adding font link
-  document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet]
-
-
+  sh.adoptedStyleSheets = [sheet]
+  // ----------------------------------------
+  return el
+  // ----------------------------------------
   function handle_page_change (active_page) {
-    PROTOCOL.active_page = active_page;
-    document.body.removeChild(current_page)
-    current_page = page_list[active_page]
-    document.body.append(current_page)
+    //   PROTOCOL.active_page = active_page;
+    content_sh.replaceChildren(page_list[active_page])
+    //   current_page = page_list[active_page]
     const message = {
       head: ['root', 'navbar', 'navbar'],
       type: 'theme',
@@ -58,18 +86,14 @@ function desktop () {
     }
     notify(message)
   }
-
   function handle_theme_change () {
     ;current_theme = current_theme === light_theme ? dark_theme : light_theme
     sheet.replaceSync(get_theme(current_theme))
   }
-
   function toggle_terminal () {
-    ;document.body.contains(terminal_wrapper) ? 
-    document.body.removeChild(terminal_wrapper) :
-    document.body.append(terminal_wrapper)
+    if (terminal_sh.contains(terminal_el)) return terminal_el.remove()
+    terminal_sh.append(terminal_el)
   }
-
   function page_protocol (handshake, send, mid = 0) {
     notify = send
 
@@ -96,12 +120,11 @@ function desktop () {
     //   })
     // }
   }
-
 }
 
 function get_theme (opts) {
   return`
-    :root{ 
+    :host { 
       --bg_color: ${opts.bg_color};
       --ac-1: ${opts.ac_1};
       --ac-2: ${opts.ac_2};
@@ -109,18 +132,14 @@ function get_theme (opts) {
       --primary_color: ${opts.primary_color};
       font-family: Silkscreen;
       color: var(--primary_color);
-    }
-    svg{
-      fill: var(--bg_color);
-    }
-    .page_filler{
       background-image: radial-gradient(var(--primary_color) 2px, var(--bg_color) 2px);
       background-size: 16px 16px;
       width: 100vw;
       height: 100vh;
-      z-index: -100;
       position: fixed;
-      top: 41px;
+    }
+    svg {
+      fill: var(--bg_color);
     }
   `
 }
